01c4caa0af85a2117b4940ef95ad2e50771520a3
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-10-21 22:33:02 +0900

    とりあえず、なんか描いて動かす

--- src/com/example/glcamerasample/CameraRenderer.java
+++ src/com/example/glcamerasample/CameraRenderer.java
@@ -1,26 +1,69 @@
 package com.example.glcamerasample;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLSurfaceView.Renderer;
 
 public class CameraRenderer implements Renderer {
+    
+    private int mCounter;
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        // TODO Auto-generated method stub
-        
+        mCounter = 0;
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // GLで利用する領域を指定
+        gl.glViewport(0, 0, width, height);
+        
+        float ratio = (float) height / width;
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        
+        // 座標系の指定
+        gl.glOrthof(-1.0f, 1.0f, -ratio, ratio, 0.5f, -0.5f);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        // TODO Auto-generated method stub
+        gl.glClearColor(0.f, 0.f, 1.f, 1.f);
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        
+        float[] vertices = {
+                -0.5f, -0.5f,
+                0.5f, -0.5f,
+                -0.5f, 0.5f,
+                0.5f, 0.5f
+        };
+        
+        FloatBuffer fb = makeFloatBuffer(vertices);
+        
+        gl.glVertexPointer(2,  GL10.GL_FLOAT, 0, fb);
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        
+        mCounter++;
+        gl.glRotatef((float) mCounter, 0.0f, 0.0f, 1.0f);
+        
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
     }
-
+    
+    private static final FloatBuffer makeFloatBuffer(float[] arr) {
+        ByteBuffer bb = ByteBuffer.allocateDirect(arr.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        FloatBuffer fb = bb.asFloatBuffer();
+        fb.put(arr);
+        fb.position(0);
+        
+        return fb;
+    }
 }
